f3f3946608f7da472a9e67d1346c74a1628daff5
Codya <codyadcock10@gmail.com>
2024-12-14 18:21:38 -0700

    First Commit of Day 1

---
+++ Day 1/task/task.py
@@ -0,0 +1,12 @@
+# # Greeting
+# print("Hello, welcome to the Band Name Generator!\n")
+# # Ask for the city they grew up in
+# city = input("What is the name of the city you grew up in?\n")
+# # Name of the pet
+# pet = input("Thanks! Now, what is the name of your first pet?\n")
+# # Give Band Name
+# print("Badass! Your band name is " + pet + " " + city + "! Or would you prefer " + city + " " + pet + "?\nI guess none of it really matters!")
+
+# One liner attempt
+print("Heya, welcome to the Band Name Generator!")
+print("Your Band Name is " +input("What is the name of your pet?\n") + " " + input("What is the name of your home town?\n") + "!\nCongrats!")
---
+++ Day 2/Data Types/task.py
@@ -0,0 +1,11 @@
+len("Hello")
+
+myString = "Hello World"
+myInt = 1
+myFloat = 1.5
+myBool = True
+
+print(type(myString))
+print(type(myInt))
+print(type(myFloat))
+print(type(myBool))
