b308df8aef9bedca435a3357896ec57e9470168b
Codya <codyadcock10@gmail.com>
2024-12-19 17:41:19 -0700

    Day 2 Commit

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,4 @@
 bmi = 84 / 1.65 ** 2
+
+flrBmi = int(bmi)
+rndBmi = round(bmi)
--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -3,4 +3,6 @@ bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 
+print("Each person should pay ${:,.2f}".format((bill +(bill*(tip/100))) / people))
+
 
